1e7c127fefdc9dda590212b6d4e360c74d830566
Anastasia Yarosh <fysyad@gmail.com>
2025-09-09 19:40:48 +0300

    Форматирование

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import 'primeicons/primeicons.css'  
+import 'primeicons/primeicons.css'
 import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-    }
+  theme: {
+    preset: Aura,
+  },
 })
 app.use(createPinia())
 app.use(router)
