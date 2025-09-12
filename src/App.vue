33c89293ee6a6f87d1fca3f895dc8053f4104c29
Anastasia Yarosh <fysyad@gmail.com>
2025-09-12 14:45:03 +0300

    Стили добавила

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
 </script>
 
 <template>
+  <header>Hacker News</header>
   <div class="container">
     <router-view></router-view>
   </div>
@@ -13,28 +13,16 @@ import { RouterView } from 'vue-router'
 .container {
   padding: 20px;
 }
-.spinner {
-  display: flex;
-}
+
 header {
-  display: flex;
-  justify-content: center;
-  margin: 20px;
-}
-.card {
   width: auto;
-  overflow: hidden;
-  padding: 20px;
-}
-i {
-  display: flex;
-  justify-content: center;
-}
-.score {
-  margin-top: 4px;
-}
-.author {
-  margin-top: 8px;
-  margin-bottom: 8px;
+  background: #3732b3;
+  height: 90px;
+  font-size: 24px;
+  font-weight: 600;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  color: white;
+  padding: 30px;
+  padding-left: 60px;
 }
 </style>
