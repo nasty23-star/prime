8d59bd114b7974b30dce3898ebe19a8663343d8a
Anastasia Yarosh <fysyad@gmail.com>
2025-09-22 22:49:21 +0300

    Переименовала стор

---
+++ src/stores/data.ts
@@ -0,0 +1,9 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { NewsItem } from '@/types/common.ts'
+
+export const useDataStore = defineStore('data', () => {
+
+
+  return {  }
+})
