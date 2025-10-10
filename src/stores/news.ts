82ad8a7cc83606d095f0ad26a3c8a76790c90b58
Anastasia Yarosh <fysyad@gmail.com>
2025-10-10 16:30:01 +0300

    Переписала функцию загрузку карточки, чтобы запрос был только если ее нет локально

--- src/stores/news.ts
+++ src/stores/news.ts
@@ -1,7 +1,7 @@
 import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { NewsItem, NHComment } from '@/types/common.ts'
-
+import { useDataStore } from './data'
 export const useNewsStore = defineStore('news', () => {
   const newsItem = ref<NewsItem | null>(null)
   const loadingComments = ref(false)
@@ -12,10 +12,27 @@ export const useNewsStore = defineStore('news', () => {
     return newsItem.value?.descendants ?? 0
   })
 
+  const dataStore = useDataStore()
+
+  // Функция для получения айди карточки новости
+  const getNewsItemFromData = (itemId: string | string[]) => {
+    const id = Number(Array.isArray(itemId) ? itemId[0] : itemId)
+    return dataStore.dataCards.find((card) => card.id === id)
+  }
+
   const fetchNewsItem = async (itemId: string | string[]) => {
     loadingNews.value = true
     try {
       const id = Array.isArray(itemId) ? itemId[0] : itemId
+
+      // Сначала пытаемся найти в локальных данных
+      const localItem = getNewsItemFromData(id)
+      if (localItem) {
+        newsItem.value = localItem
+        return
+      }
+
+      // Если не нашли локально, делаем запрос к API
       const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
       newsItem.value = await response.json()
     } catch (error) {
@@ -85,6 +102,7 @@ export const useNewsStore = defineStore('news', () => {
     }
   }
   return {
+    getNewsItemFromData,
     newsItem,
     refreshComments,
     loadingComments,
