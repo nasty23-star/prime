86e54dc13c0f76f9315c0ae4f873be7e7ffed627
Anastasia Yarosh <fysyad@gmail.com>
2025-11-11 20:24:59 +0300

    Адаптивность

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { ref, computed, onMounted, onUnmounted } from 'vue'
+
+const screenWidth = ref(window.innerWidth)
+
+const updateWidth = () => {
+  screenWidth.value = window.innerWidth
+}
+
+const isTabletAndMore = computed(() => screenWidth.value > 600)
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth)
+})
 </script>
 
 <template>
   <div class="wrapper">
     <header>
-      <div class="header">Hacker News</div>
-      <nav class="navigation">
-        <router-link to="/favourite" class="nav-link" :class="{ active: $route.path === '/home' }"
-          ><i class="pi pi-heart icon"></i> Избранное</router-link
-        >
-        <router-link to="/" class="nav-link">
-          <i class="pi pi-home icon"></i>
-          Главная</router-link
-        >
-      </nav>
+      <div class="header-container">
+        <div class="header">Hacker News</div>
+        <nav class="navigation">
+          <router-link to="/favourite" class="nav-link" :class="{ active: $route.path === '/home' }"
+            ><i class="pi pi-heart icon"></i>
+            <span v-if="isTabletAndMore">Избранное</span></router-link
+          >
+          <router-link to="/" class="nav-link">
+            <i class="pi pi-home icon"></i>
+            <span v-if="isTabletAndMore">Главная</span></router-link
+          >
+        </nav>
+      </div>
     </header>
     <div class="container">
       <router-view></router-view>
@@ -30,7 +50,6 @@ header {
   font-size: 24px;
   font-weight: 600;
   color: white;
-  padding-left: 60px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -39,12 +58,21 @@ header {
   z-index: 1000;
 }
 
+.header-container {
+  padding-left: 10px;
+  padding-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1100px;
+  margin: auto;
+}
+
 nav {
   gap: 10px;
-  padding-right: 60px;
   height: 70px;
   display: flex;
-
   align-items: center;
   font-weight: 500;
   font-size: 15px;
@@ -54,9 +82,10 @@ nav {
     flex-direction: column;
     align-items: center;
     padding-bottom: 4px;
-    padding-top: 4px;
+    padding-top: 10px;
     text-decoration: none;
-    width: 86px;
+    width: 40px;
+    height: 40px;
     color: #ffffff;
   }
 
@@ -76,6 +105,18 @@ nav {
 @media (min-width: 420px) {
   .container {
     padding: 20px;
+  }
+}
+
+@media (min-width: 600px) {
+  .header-container {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+  nav {
+    a {
+      width: 86px;
+    }
   }
 }
 </style>
